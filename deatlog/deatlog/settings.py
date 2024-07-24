92b9a92165ade5ca6782a48eb4468ad4b176eac9
kod-python <kelvinlik0245@gmail.com>
2024-07-24 15:14:27 +0000

    django sms

--- deatlog/deatlog/settings.py
+++ deatlog/deatlog/settings.py
@@ -192,13 +192,12 @@ CORS_EXPOSE_HEADERS = [
 ]
 
 
-
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'kelalice931@gmail.com'
-EMAIL_HOST_PASSWORD = 'kelvinalive8$!'
+EMAIL_HOST_PASSWORD = 'jdceuknxumjvtjxv'
 DEFAULT_FROM_EMAIL = 'your_email@gmail.com'
 FRONTEND_URL = 'http://localhost:3000'
 
--- deatlog/trial/serializers.py
+++ deatlog/trial/serializers.py
@@ -45,7 +45,11 @@ class UserSignupSerializer(serializers.ModelSerializer):
     
     
     
-    
+    def validate(self, data):
+        if data['password'] != data['confirm_password']:
+            raise serializers.ValidationError("Passwords do not match.")
+        return data
+
     
     
     def create(self, validated_data):
