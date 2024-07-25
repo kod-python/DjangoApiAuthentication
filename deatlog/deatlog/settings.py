e9e6c1ac68dc812807b2b243ba2200421a598a60
kod-python <kelvinlik0245@gmail.com>
2024-07-25 08:48:20 +0000

    changes made

--- deatlog/deatlog/settings.py
+++ deatlog/deatlog/settings.py
@@ -192,23 +192,16 @@ CORS_EXPOSE_HEADERS = [
 ]
 
 
+
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'kelalice931@gmail.com'
-EMAIL_HOST_PASSWORD = 'jdceuknxumjvtjxv'
+EMAIL_HOST_USER = 'your_email@gmail.com'
+EMAIL_HOST_PASSWORD = 'your_email_password'
 DEFAULT_FROM_EMAIL = 'your_email@gmail.com'
 FRONTEND_URL = 'http://localhost:3000'
-
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = 'your_email@gmail.com'
-# EMAIL_HOST_PASSWORD = 'your_email_password'
-# DEFAULT_FROM_EMAIL = 'your_email@gmail.com'
-# FRONTEND_URL = 'http://localhost:3000'
 
 
 
